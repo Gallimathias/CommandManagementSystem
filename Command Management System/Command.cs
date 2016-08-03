792cf04ce399c4afa9c512f2d125e45647370dfd
Marcus Aurelius <Marcus.Aurelius@gallimathias.de>
2016-08-03 18:21:06 +0200

    Initialization: Stand of NoobDev Bot project (d4ce0247)

---
+++ Command Management System/Command.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoMaS
+{
+    public class Command<TParameter, TOut>
+    {
+        public Func<TParameter, TOut> NextFunction { get; set; }
+
+        public TOut Dispatch(TParameter parameter) => NextFunction(parameter);
+
+        public delegate void FinishEventHandler(object sender, TParameter e);
+
+        public event FinishEventHandler FinishEvent;
+
+        public void RaiseFinishEvent(object sender, TParameter e) => FinishEvent?.Invoke(sender, e);
+
+    }
+}
