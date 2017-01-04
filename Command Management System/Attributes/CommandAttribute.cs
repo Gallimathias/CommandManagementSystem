0b07cfe283e4583a7e8c764d3cd4913a607e2c6a
Marcus Aurelius <Marcus.Aurelius@gallimathias.de>
2017-01-04 15:35:48 +0100

    Command Manager System: Add Attributes

---
+++ Command Management System/Attributes/CommandAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoMaS.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class, Inherited = true)]
+    public class CommandAttribute : Attribute
+    {
+        public readonly object Tag;
+        public readonly string[] Aliases;
+
+        public CommandAttribute(object tag, params string[] aliases)
+        {
+            Tag = tag;
+            Aliases = aliases;
+        }
+    }
+}
---
+++ Command Management System/Attributes/CommandManagerAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoMaS.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class, Inherited = true)]
+    public class CommandManagerAttribute : Attribute
+    {
+        public readonly object Tag;
+        public readonly string[] CommandNamespaces;
+
+        public CommandManagerAttribute(object tag)
+        {
+            Tag = tag;
+        }
+
+        public CommandManagerAttribute(object tag, params string[] commandNamespaces)
+        {
+            Tag = tag;
+            CommandNamespaces = commandNamespaces;
+        }
+    }
+}
