e542b6838829288d0065dee25c5e4caa79648739
Maximilian Krüger <Maximilian.Krueger@it.konicaminolta.de>
2017-05-18 10:54:26 +0200

    #5 Add new execution logic for commands

--- Command Management System/Command.cs
+++ Command Management System/Command.cs
@@ -1,7 +1,9 @@
-﻿using CommandManagementSystem.Interfaces;
+﻿using CommandManagementSystem.Attributes;
+using CommandManagementSystem.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,6 +16,11 @@ namespace CommandManagementSystem
     /// <typeparam name="TOut">The data type of the dispatchresponse</typeparam>
     public abstract class Command<TParameter, TOut> : ICommand<TParameter, TOut>
     {
+        public static string[] ExecutionOrder { get; protected set; }
+        public static bool Registered => registered && ExecutionOrder != null && ExecutionOrder?.Length > 0;
+        private static bool registered;
+        protected int executionCount;
+
         /// <summary>
         /// Contains the delegates for the next function to execute
         /// </summary>
@@ -38,21 +45,41 @@ namespace CommandManagementSystem
 
         public Command()
         {
-            NextFunction = Main;
+            executionCount = 0;
         }
 
         public virtual TOut Main(TParameter arg)
         {
+            NextFunction = null;
             return default(TOut);
         }
-
 
         /// <summary>
         /// Executes the next action in the command
         /// </summary>
         /// <param name="arg">Parameters to be passed</param>
         /// <returns>Returns a fixed return value</returns>
-        public virtual TOut Dispatch(TParameter arg) => NextFunction(arg);
+        public virtual TOut Dispatch(TParameter arg)
+        {
+            if (Registered)
+                NextFunction = (Func<TParameter, TOut>)Delegate.CreateDelegate(
+                    typeof(Func<TParameter, TOut>), this, ExecutionOrder[executionCount]);
+            else
+                NextFunction = Main;
+
+            executionCount++;
+            var returnValue = NextFunction(arg);
+            if (Registered)
+                if (executionCount < ExecutionOrder.Length)
+                    RaiseWaitEvent(this, Dispatch);
+                else
+                    NextFunction = null;
+            
+            if (NextFunction == null)
+                RaiseFinishEvent(this, arg);
+
+            return returnValue;
+        }
 
         /// <summary>
         /// Initializes the command
@@ -67,13 +94,40 @@ namespace CommandManagementSystem
         /// <param name="sender">This command</param>
         /// <param name="arg">The passed parameters</param>
         public virtual void RaiseFinishEvent(object sender, TParameter arg) => FinishEvent?.Invoke(sender, arg);
-        
+
         /// <summary>
         /// Raises the wait event
         /// </summary>
         /// <param name="sender">This command</param>
         /// <param name="arg">The dispatch method</param>
         public virtual void RaiseWaitEvent(object sender, Func<TParameter, TOut> arg) => WaitEvent?.Invoke(sender, arg);
+
+        public static void Registration()
+        {
+            var actions = MethodBase.GetCurrentMethod().DeclaringType.GetMembers()?.Where(
+                m => m.GetCustomAttribute<NextAttribute>() != null)?.ToArray();
+
+            if (actions == null || actions?.Length == 0)
+                return;
+
+            var list = new List<KeyValuePair<NextAttribute, MemberInfo>>();
+
+            ExecutionOrder = new string[actions.Length];
+
+            for (int i = 0; i < actions.Length; i++)
+            {
+                var attr = actions[i].GetCustomAttribute<NextAttribute>();
+                list.Add(new KeyValuePair<NextAttribute, MemberInfo>(attr, actions[i]));
+            }
+
+            list.OrderBy(b => b.Key.Order);
+
+            for (int i = 0; i < list.Count; i++)
+                ExecutionOrder[i] = list[i].Value.Name;
+
+
+            registered = true;
+        }
     }
 
     /// <summary>
