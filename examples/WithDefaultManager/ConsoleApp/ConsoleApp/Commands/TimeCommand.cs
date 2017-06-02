151079af41a1938aae1dbfd143e56d9443252542
Marcus Aurelius <Marcus.Aurelius@gallimathias.de>
2017-06-02 16:07:08 +0200

    Add example with the DefaultCommandManager

---
+++ examples/WithDefaultManager/ConsoleApp/ConsoleApp/Commands/TimeCommand.cs
@@ -0,0 +1,18 @@
+﻿using CommandManagementSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Commands
+{
+    public class TimeCommand : Command<object[]>
+    {
+        public override dynamic Main(object[] arg)
+        {
+            Console.WriteLine(DateTime.Now);
+            return base.Main(arg);
+        }
+    }
+}
---
+++ examples/WithDefaultManager/ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using CommandManagementSystem;
+using CommandManagementSystem.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static DefaultCommandManager defaultCommandManager;
+
+        static void Main(string[] args)
+        {
+            defaultCommandManager = new DefaultCommandManager("ConsoleApp.Commands", "ConsoleApp");
+
+            foreach (var command in args)
+                defaultCommandManager.Dispatch(command, null);
+
+            Console.ReadKey();
+        }
+
+        [OneTimeCommand("/hello")]
+        static dynamic Hello(object[] arg)
+        {
+            Console.WriteLine("Hello");
+            return null;
+        } 
+    }
+}
