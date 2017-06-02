78585bea542b4393c964ef5eaed955043bde2438
Marcus Aurelius <Marcus.Aurelius@gallimathias.de>
2017-06-02 16:33:59 +0200

    Add example with customized CommandManager

---
+++ examples/WithCustomizeManager/ConsoleApp/ConsoleApp/Commands/OneTimeCommands.cs
@@ -0,0 +1,19 @@
+﻿using CommandManagementSystem.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Commands
+{
+    static class OneTimeCommands
+    {
+        [OneTimeCommand("/hello")]
+        public static bool Hello(string arg)
+        {
+            Console.WriteLine("Hello");
+            return true;
+        }
+    }
+}
---
+++ examples/WithCustomizeManager/ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static ConsoleManager manager;
+
+        static void Main(string[] args)
+        {
+            manager = new ConsoleManager();
+
+            foreach (var command in args)
+            {
+                if (!manager.Dispatch(command, "Sweet Home Sweet"))
+                    throw new Exception("Command not succe");
+
+            }
+        }
+    }
+}
