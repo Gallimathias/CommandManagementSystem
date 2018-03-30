5acc66406bafcb517d63f54cdd909a2cfc0f5a25
Maximilian Krüger <Marcus.Aurelius@gallimathias.de>
2018-03-30 16:32:02 +0200

    #30 Add DefaultManagerTests

---
+++ CommandManagementSystemTest/DefaultManagerTest.cs
@@ -0,0 +1,28 @@
+﻿using CommandManagementSystem;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandManagementSystemTest
+{
+    [TestClass]
+    public class DefaultManagerTest
+    {
+        [TestMethod]
+        public void DefaultManagerInstance()
+        {
+            var manager = new DefaultCommandManager<string, int>("CommandManagementSystemTest");
+
+            for (int i = 0; i < 10; i++)
+            {
+                Assert.AreEqual(1, manager.Dispatch("test", ""));
+                Assert.AreEqual(2, manager.Dispatch("test", ""));
+                Assert.AreEqual(1, manager.Dispatch("t", ""));
+                Assert.AreEqual(2, manager.Dispatch("t", ""));
+            }
+        }
+    }
+}
